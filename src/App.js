110a8ac2032919623aed583471c7d8e25aeaf86c
Nina Palma <ninapalmadev@gmail.com>
2023-05-22 17:34:50 -0300

    erro do lint

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom/cjs/react-router-dom.min';
 import Home from './components/Home';
 import './App.css';
+
 function App() {
   return (
     <Switch>
