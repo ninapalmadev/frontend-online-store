a7bac1e2f668e424c62218c8cf7ee7426ba5b04d
Nina Palma <ninapalmadev@gmail.com>
2023-05-22 17:25:17 -0300

    altera app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom/cjs/react-router-dom.min';
-import Home from './components/Home';
+import logo from './logo.svg';
 import './App.css';
+
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={ Home } />
-    </Switch>
+    <div className="App">
+      <header className="App-header">
+        <img src={ logo } className="App-logo" alt="logo" />
+        <p>Edit src/App.js and save to reload.</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
+
 export default App;
